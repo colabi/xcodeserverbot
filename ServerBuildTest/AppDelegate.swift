0cadfd844d7992109f820aa32344d224a6e742f2
seth piezas <spiezas@exo-imaging.com>
2019-06-23 20:55:01 -0700

    asd

--- ServerBuildTest/AppDelegate.swift
+++ ServerBuildTest/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         let ans = h.hello()
-        print("loading another thing adding cover coverage: \(ans + 2)")
+        print("test another thing adding cover coverage: \(ans + 2)")
         
     }
 
